3c82d0b1c5d9f030788479a7a8cd7d8ab76a13dd
Alexander Dickson <alex@alexanderdickson.com>
2016-02-02 20:51:41 +0100

    Better way of handling fzf

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ for file in vimrc gitconfig gitignore zshrc; do
 	echo "$file linked to ~/.$file"
 done
 
-if uname -a | grep -q Darwin; then
-    brew install fzf
-else
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
-fi
+# Install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
--- zshrc
+++ zshrc
@@ -8,3 +8,5 @@ source $ZSH/oh-my-zsh.sh
 
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 
+# Enable fzf
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
