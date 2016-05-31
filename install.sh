6876ca6b11d8aae701742bc4efdf7d0d36ea1544
Alexander Dickson <alex@alexanderdickson.com>
2016-05-31 14:05:04 +0200

    Save some traffic

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ done
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
